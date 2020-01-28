933b20f99854b4e0a4efb8001d52026ef86518f8
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-01-28 09:32:12 -0500

    Corrected the jar file path

--- spark/spark_process.sh
+++ spark/spark_process.sh
@@ -1,1 +1,5 @@
-/usr/local/spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.9 --jars /home/ubuntu/usrlib/postgresql-42.2.9.jar preprocess_store_data.py
+#!/bin/bash
+
+/usr/local/spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.9 --jars /home/ubuntu/data-warehouse-solution/usrlib/postgresql-42.2.9.jar preprocess_store_data.py
+
+
