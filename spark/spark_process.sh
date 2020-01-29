9e58f8b6e1ceb5b431f62cdee7586859e4b324d9
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-01-28 20:57:19 -0500

    added spark master IP option

--- spark/spark_process.sh
+++ spark/spark_process.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-/usr/local/spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.9 --jars /home/ubuntu/data-warehouse-solution/usrlib/postgresql-42.2.9.jar preprocess_store_data.py
+/usr/local/spark/bin/spark-submit --master spark://10.0.0.8:7077\
+                                  --jars /home/ubuntu/data-warehouse-solution/usrlib/postgresql-42.2.9.jar\
+                                  preprocess_store_data.py
 
 
