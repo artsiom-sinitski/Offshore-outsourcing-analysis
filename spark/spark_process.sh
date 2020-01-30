9d0798d5fe349d4873b5f1fe6adb44431ce8a824
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-01-29 21:28:12 -0500

    minor formatting changes

--- spark/spark_process.sh
+++ spark/spark_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/spark/bin/spark-submit --master spark://10.0.0.8:7077\
-                                  --jars /home/ubuntu/data-warehouse-solution/usrlib/postgresql-42.2.9.jar\
+                                  --jars /home/ubuntu/usrlib/postgresql-42.2.9.jar\
                                   preprocess_store_data.py
 
 
