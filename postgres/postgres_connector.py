398fd74980c7fca959656c68645db0bb61eaca48
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-01-31 21:50:47 -0500

    changed the ip address

--- postgres/postgres_connector.py
+++ postgres/postgres_connector.py
@@ -1,15 +1,17 @@
 import logging
-# import psycopg2
 from pyspark.sql import DataFrameWriter
+
 
 class PostgresConnector(object):
     def __init__(self):
         self.database = 'postgres'
         self.username = 'postgres'
         self.password = ''
-        self.host = '10.0.0.14'
+        self.host = '10.0.0.04'
         self.port = '5432'
-        self.url = 'jdbc:postgresql://{host}:{port}/{db}'.format(host=self.host, port=self.port, db=self.database)
+        self.url = 'jdbc:postgresql://{host}:{port}/{db}'.format(host=self.host,\
+                                                                 port=self.port,\
+                                                                   db=self.database)
         self.properties = { "user"     : self.username, 
                             "password" : self.password,
                             "driver"   : "org.postgresql.Driver"
