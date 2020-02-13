c1d67d2a409689ca03517e713a7e64605300b613
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-02-13 00:41:13 -0500

    renamed files

--- spark/spark_process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/local/spark/bin/spark-submit --master spark://10.0.0.8:7077\
-                                  --jars /home/ubuntu/usrlib/postgresql-42.2.9.jar\
-                                  preprocess_save_data.py
-
-
