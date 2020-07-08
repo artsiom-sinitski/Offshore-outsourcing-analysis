799d7e8481b8f534550a3e1db1faa1fe2ee013db
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-07-08 16:17:32 +0000

    extended postgres connector functionality to handle reads/writes by central storage and datamart

---
+++ spark/constants.py
@@ -0,0 +1,11 @@
+"""
+Purpose: This class defines various constant variables.  
+Author: Artsiom Sinitski
+Email:  artsiom.vs@gmail.com
+"""
+from enum import Enum
+
+
+class EndPoint(Enum):
+    CENTRAL_STORAGE = "central_storage"
+    PROTESTS_DATAMART = "protests_datamart"
