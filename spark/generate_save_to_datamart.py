5761114eeea120a3ed6f43c95192c62c49179da8
Artsiom Sinitski <artsiom.vs@gmail.com>
2020-07-09 17:55:42 +0000

    automated table initializationf, still need to add all the tables to the dictionary

--- spark/generate_save_to_datamart.py
+++ spark/generate_save_to_datamart.py
@@ -27,6 +27,10 @@ class GenerateAndSaveDataToDatamart(object):
         # self.date_from = date_from
         # self.date_to = date_to
         self.mode = "append"
+
+        self.dm_cols_dict = dict()
+        self.dm_row_id_names_dict = dict()
+        self.initialize_dm_columns_dictionary()
         
         self.spark = SparkSession \
                     .builder \
@@ -35,56 +39,141 @@ class GenerateAndSaveDataToDatamart(object):
                     .getOrCreate()
 
 
+    #TODO: add columns for all the tables
+    def initialize_dm_columns_dictionary(self):
+        """[summary]
+        """
+        dm_table_name = "Event_Fact"
+        self.dm_cols_dict[dm_table_name] = ["GoldsteinScale", "NumMentions", "NumSources", \
+                                           "NumArticles", "AvgTone", "SqlDate", \
+                                           "DateAdded", "GlobalEventId"]
+        
+        dm_table_name = "Event_Dim"
+        self.dm_cols_dict["Event_Dim"] = ["IsRootEvent", "EventCode", "EventBaseCode", \
+                                          "EventRootCode", "QuadClass", "SourceUrl"]
+        self.dm_row_id_names_dict[dm_table_name] = "event_id"
+
+        dm_table_name = "Actor1_Dim"
+        self.dm_cols_dict[dm_table_name] = ["Actor1Code", "Actor1Name", "Actor1CountryCode", \
+                                           "Actor1KnownGroupCode", "Actor1EthnicCode", "Actor1Religion1Code", \
+                                           "Actor1Religion2Code", "Actor1Type1Code"]
+        self.dm_row_id_names_dict[dm_table_name] = "actor1_id"
+
+        #dm_table_name = "Actor2_Dim"
+        # self.dm_cols_dict["Actor2_Dim"] = ["Actor2Code", "Actor2Name", "Actor2CountryCode", \
+        #                                     "Actor2KnownGroupCode", "Actor2EthnicCode", "Actor2Religion1Code", \
+        #                                     "Actor2Religion2Code", "Actor2Type1Code"]
+        #self.dm_row_id_names_dict[dm_table_name] = "actor2_id"
+
+        #dm_table_name = "Actor1Geo_Dim"
+
+
     #TODO:
     # (1) - need to read data by a date period and save it to a parquet file
     # select count group by date to determine the size of the date slice
+    # (2) pass query from the outside
     def read_data_from_central_storage(self):
-        query = "(select * from gdelt_events where \"EventRootCode\" like '14%' limit 17) ge"
+        query = "(select * from gdelt_events where \"EventRootCode\" like '14%' limit 3) ge"
 
         connector = PostgresConnector(EndPoint.CENTRAL_STORAGE.value)
         df = connector.read_from_db(self.spark, query)
-
-        # The line below works as expected:
-        #   df = self.spark.read.jdbc(url=url, table=query, properties=properties)
-        # Also, it should work how it is written below but it doesn't:
-        #   df = DataFrameReader(self.spark).jdbc(url=url, table=query, properties=properties)
-        #see https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrameReader
 
         # df.printSchema()
         df.show(3, True)
         return df
 
 
-    #TODO
+    #TODO:
     # (2) - add the rest of the star schema tables
+    # (1) - base id should be the max row_id from the Event_Fact table
     def populate_datamart_tables(self, df):
-        dm_table_dict = dict()
+        dm_tables_dict = dict()
         #generate a pseudorandom base id -> random int from (1, 9) range || seconds
         base_id = datetime.now().strftime(str(randrange(1,9)) + "%S.%f").replace('.', '')[:-2]
+        unique_id = base_id + F.monotonically_increasing_id()
+        row_id = F.lit(unique_id)
 
-        # =========== Event_Dim table ======================= 
-        cols = ["IsRootEvent", "EventCode", "EventBaseCode", \
-                "EventRootCode", "QuadClass", "SourceUrl"]
-        event_dim_id = base_id + F.monotonically_increasing_id()
-        #where_clause = "\"EventRootCode\" like '14%'"
-        event_dim_df = df.select(*cols).withColumn("event_id", F.lit(event_dim_id))
+        # loop through the dict that holds columns for appropriate tables
+        for table in self.dm_cols_dict.keys():
+            cols = self.dm_cols_dict[table]
+            if table == "Event_Fact":
+                tmp_df = df.select(*cols).withColumn("event_id", row_id) \
+                                         .withColumn("actor1_id", row_id) \
+                                         .withColumn("actor2_id", F.lit("null")) \
+                                         .withColumn("actor1geo_id", F.lit("null")) \
+                                         .withColumn("actor2geo_id", F.lit("null")) \
+                                         .withColumn("actiongeo_id", F.lit("null"))
+            else:
+                tmp_df = df.select(*cols).withColumn(self.dm_row_id_names_dict[table], row_id)
+            
+            dm_tables_dict[table] = tmp_df
 
-        #event_dim_df.printSchema()
-        print("\n=========================================================\n")
-        #event_dim_df.explain()
-        print("\n*** DataFrame count: ", event_dim_df.count())
-        print("\n=========================================================\n")
-        event_dim_df.show(5, True)
+            print("\n*** " + table + " DataFrame count: ", tmp_df.count(), end='\n')
+            tmp_df.show(5, True)
 
-        dm_table_dict["Event_Dim"] = event_dim_df
-        return dm_table_dict
+        # ==================== Event_Dim table ====================================
+        # ========================================================================= 
+        # cols = ["IsRootEvent", "EventCode", "EventBaseCode", \
+        #         "EventRootCode", "QuadClass", "SourceUrl"]
+        # event_id = F.lit(unique_id)
+        # event_dim_df = df.select(*cols).withColumn("event_id", event_id)
+
+        # #event_dim_df.printSchema()
+        # #event_dim_df.explain()
+        # print("\n=========================================================\n")
+        # print("\n*** Event_Dim DataFrame count: ", event_dim_df.count())
+        # print("\n=========================================================\n")
+        # event_dim_df.show(5, True)
+
+        # dm_tables_dict["Event_Dim"] = event_dim_df
+
+        # # ==================== Actor1_Dim table =================================
+        # # =======================================================================
+        # cols = ["Actor1Code", "Actor1Name", "Actor1CountryCode", "Actor1KnownGroupCode", \
+        #         "Actor1EthnicCode", "Actor1Religion1Code", "Actor1Religion2Code", "Actor1Type1Code"]
+        # actor1_id = F.lit(unique_id)
+        # actor1_dim_df = df.select(*cols).withColumn("actor1_id", actor1_id)
+
+        # #event_dim_df.printSchema()
+        # print("\n=========================================================\n")
+        # print("\n*** Actor1_Dim DataFrame count: ", actor1_dim_df.count())
+        # print("\n=========================================================\n")
+        # actor1_dim_df.show(5, True)
+
+        # dm_tables_dict["Actor1_Dim"] = actor1_dim_df
+
+        # # ==================== Event_Fact table =================================
+        # # =======================================================================
+        # cols = ["GoldsteinScale", "NumMentions", "NumSources", "NumArticles", \
+        #         "AvgTone", "SqlDate", "DateAdded", "GlobalEventId"]
+        # event_fact_df = df.select(*cols).withColumn("event_id", event_id) \
+        #                                 .withColumn("actor1_id", actor1_id) \
+        #                                 .withColumn("actor2_id", F.lit("null")) \
+        #                                 .withColumn("actor1geo_id", F.lit("null")) \
+        #                                 .withColumn("actor2geo_id", F.lit("null")) \
+        #                                 .withColumn("actiongeo_id", F.lit("null"))
+
+        # #event_dim_df.printSchema()
+        # print("\n=========================================================\n")
+        # print("\n*** Event_Fact DataFrame count: ", event_fact_df.count())
+        # print("\n=========================================================\n")
+        # event_fact_df.show(5, True)
+
+        # dm_tables_dict["Event_Fact"] = event_fact_df
+
+        return dm_tables_dict
 
 
-    def save_tables_to_datamart(self, dm_table_dict):
+    def save_tables_to_datamart(self, dm_tables_dict):
+        """[summary]
+
+        Args:
+            dm_tables_dict ([type]): [description]
+        """
         connector = PostgresConnector(EndPoint.PROTESTS_DATAMART.value)
 
-        for table in dm_table_dict.keys():
-            df = dm_table_dict[table]
+        for table in dm_tables_dict.keys():
+            df = dm_tables_dict[table]
             connector.write_to_db(df, table, self.mode)
 
         print("\n***** Saved dataframe to Datamart! *****\n")
@@ -94,9 +183,9 @@ class GenerateAndSaveDataToDatamart(object):
     #(1) - loop through the data based on the date range
     def run(self):
         dw_df = self.read_data_from_central_storage()
-        dm_table_dict = self.populate_datamart_tables(dw_df)
-        #print("\n****** DataFrames Dict content: ", dm_table_dict)
-        self.save_tables_to_datamart(dm_table_dict)
+        dm_tables_dict = self.populate_datamart_tables(dw_df)
+        #print("\n****** DataFrames Dict content: ", dm_tables_dict)
+        self.save_tables_to_datamart(dm_tables_dict)
 
 ################## End Of GenerateAndSaveDataToDatamart class ################
 
